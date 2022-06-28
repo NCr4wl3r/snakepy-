a0e7031a2a5d293133b5d9499b131cb9db36ae68
Espartaco <cr4wl3r@gmail.com>
2022-06-28 23:59:29 +0200

    added move restrictions

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.1)
     snake.move()
 
 
--- snake.py
+++ snake.py
@@ -1,14 +1,19 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
+LEFT = 180
+UP = 90
+RIGHT = 0
+DOWN = 270
 
 
 class Snake:
     def __init__(self) -> None:
-        self.snake = []
+        self.segments = []
         for ind in range(3):
             self.segment = self.create_body_square(pos_x=-20 * ind, pos_y=0)
-            self.snake.append(self.segment)
+            self.segments.append(self.segment)
+        self.head = self.segments[0]
 
     def create_body_square(self, pos_x, pos_y):
         snake_body = Turtle(shape="square")
@@ -18,22 +23,26 @@ class Snake:
         return snake_body
 
     def move(self):
-        for index in range(len(self.snake) - 1, 0, -1):
+        for index in range(len(self.segments) - 1, 0, -1):
             # move every section to occupy the place of the next one
-            new_position = self.snake[index - 1].pos()
-            self.snake[index].goto(new_position)
+            new_position = self.segments[index - 1].pos()
+            self.segments[index].goto(new_position)
 
         # move the head fo the snake 20 pixels to move forward
-        self.snake[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.snake[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.snake[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.snake[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.snake[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
